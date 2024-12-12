7cb722e8ada827c1e72c0147223b22edeba31755
warakornkn98 <warakornkn98@gmail.com>
2024-12-12 10:06:00 +0700

    demo 2.3 printer

--- frontend/src/component/Banner.js
+++ frontend/src/component/Banner.js
@@ -9,16 +9,28 @@ const Banner = () => {
       style={{
         background: "#1890ff",
         color: "#fff",
-        padding: "50px 20px",
+        padding: "30px 10px", // ลด Padding
         textAlign: "center",
+        maxWidth: "1200px", // กำหนดขนาดสูงสุด
+        margin: "0 auto", // จัดให้อยู่ตรงกลางหน้าจอ
+        borderRadius: "12px", // เพิ่มมุมโค้ง
       }}
     >
       <Image
         preview={false}
         src="https://scontent-bkk1-1.xx.fbcdn.net/v/t39.30808-6/468874899_996379619201327_5751534091052327281_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFHufsMQOygGIqyTFSHZffUu25pJpnngKe7bmkmmeeAp86Eh94TDHnKZL4nZdXFB_LXd-byqnlrNJGgUTBwCQcT&_nc_ohc=U4o3Z_jQYwQQ7kNvgEjhF78&_nc_zt=23&_nc_ht=scontent-bkk1-1.xx&_nc_gid=A1KvYVdwN0dULWp3RSK2XYh&oh=00_AYDNrbhDA0yhmLBpvBBPXnc10wHkaNOwzXQEftjyKgnkPw&oe=675C2AB1"
         alt="Banner"
-        style={{ marginBottom: "20px", borderRadius: "8px" }}
+        style={{
+          marginBottom: "20px",
+          borderRadius: "8px",
+          width: "100%", // ใช้ความกว้างเต็มพื้นที่
+          maxWidth: "1200px", // ขนาดสูงสุด
+          height: "auto", // ปรับสัดส่วนตามความกว้าง
+        }}
       />
+      <Title level={3} style={{ color: "#fff" }}>
+        Welcome to Our Website
+      </Title>
     </div>
   );
 };
